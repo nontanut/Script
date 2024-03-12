e6ae96eda041492b4cf4d8143ab053e04d4dc6d6
Ptest <bo.nontanut@gmail.com>
2024-03-12 15:56:26 +0700

    90D BNN Shop

---
+++ 90D.py
@@ -0,0 +1,45 @@
+from datetime import timedelta
+import pandas as pd
+
+
+import_file = input("File path\n").replace("& ","").replace("'","")
+
+try:
+    df = pd.read_excel(import_file, sheet_name="90 Day", keep_default_na=False)
+    df_dup = pd.read_excel(import_file, sheet_name="90 Day", keep_default_na=False)
+
+    # add premium
+    df["Premium"] = 20
+
+    # add stamp
+    df["Stamp"] = round(df["Premium"] * 0.004,2)
+    # add vat
+    df["Vat"] = round((df["Premium"] + df["Stamp"]) * 7/100,2)
+    # add total
+    df["Total"] = round(df["Premium"] + df["Stamp"] + df["Vat"],2)
+    # delete duplicate
+    df = df.drop_duplicates(subset=["IMEI สินค้า"], keep='last')
+    # convert date
+    str_to_date = pd.to_datetime(df["วันลงทะเบียน"],dayfirst=True)
+    df["วันลงทะเบียน"] = str_to_date
+    date = pd.to_datetime(str_to_date, format="%d%m%Y", errors="coerce")
+    ninety_day = timedelta(days=90)
+    end_day = date + ninety_day
+
+    # insert colum date
+    df.insert(13, "วันสิ้นสุด", end_day)
+
+    # add sheet dup with duplicate value
+    df1 = df_dup.sort_index(ascending=False)
+    df_dup = df_dup.drop(df1[df1["IMEI สินค้า"].duplicated() == False].index, inplace=False)
+
+
+    # export file output
+    with pd.ExcelWriter("output.xlsx") as writer:
+        df.to_excel(writer, sheet_name="90D")
+        df_dup.to_excel(writer, sheet_name="Dup")
+
+    print("Success")
+
+except Exception as e:
+    print("Error! Please Check.", e)
--- sevenYear.py
+++ sevenYear.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from datetime import timedelta
 
-o = input("File path\n")
+o = input("File path\n").replace("& ","").replace("'","")
 
 try: 
     df = pd.read_excel (o, keep_default_na=False)
@@ -28,7 +28,7 @@ try:
     df["Vat"] = round((df["Premium"] + df["Stamp"]) * 7/100,2)
     # add total
     df["Total"] = round(df["Premium"] + df["Stamp"] + df["Vat"],2)
-    # delte duplicate
+    # delete duplicate
     df = df.drop_duplicates(subset=['IMEI สินค้า'], keep='last')
     # convert date
     date = pd.to_datetime(df["วันลงทะเบียน"], format="%d/%m/%Y", errors="coerce")
@@ -42,11 +42,11 @@ try:
     df_dup = df_dup.drop(df1[df1["IMEI สินค้า"].duplicated() == False].index, inplace=False)
 
     # export
-    with pd.ExcelWriter('output.xlsx') as writer:  # doctest: +SKIP
+    with pd.ExcelWriter('output.xlsx') as writer:
         df.to_excel(writer, sheet_name='7Y')
         df_dup.to_excel(writer, sheet_name='Dup')
 
     print("Success")
 
-except:
-    print("Some Mistake")
+except Exception as e:
+    print("Some Mistake, Please Check File.",e)
